d14d25290d1d5062f9021c03c2d046b5d691f5f3
Adithya Balaji <adithyabsk@gmail.com>
2018-06-29 14:16:20 -0400

    Setup package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='foreshadow',
+      version='0.1',
+      description='',
+      url='https://github.com/georgianpartners/foreshadow',
+      author='Adithya Balaji, Alexander Allen',
+      author_email='',
+      license='Apache-2.0',
+      packages=['foreshadow'],
+      zip_safe=False)
