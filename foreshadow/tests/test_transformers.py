2431adecb13752aedb2f4257d51ddd9cb560deab
Alexander Allen <admin@altechcode.com>
2018-07-13 16:09:31 -0400

    Finish transformer implementation

---
+++ foreshadow/tests/test_transformers.py
@@ -0,0 +1,310 @@
+import pytest
+
+
+def test_transformer_wrapper_init():
+
+    from ..transformers import StandardScaler
+
+    scaler = StandardScaler(name="test-scaler", keep_columns=True)
+
+    assert scaler.name == "test-scaler"
+    assert scaler.keep_columns == True
+
+
+def test_transformer_wrapper_function():
+
+    from ..transformers import StandardScaler as CustomScaler
+    from sklearn.preprocessing import StandardScaler as StandardScaler
+    import pandas as pd
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    custom = CustomScaler()
+    sklearn = StandardScaler()
+
+    custom.fit(df[["crim"]])
+    sklearn.fit(df[["crim"]])
+
+    custom_tf = custom.transform(df[["crim"]])
+    sklearn_tf = custom.transform(df[["crim"]])
+
+    assert custom_tf.equals(sklearn_tf)
+
+    custom_tf = custom.fit_transform(df[["crim"]])
+    sklearn_tf = sklearn.fit_transform(df[["crim"]])
+
+    assert custom_tf.equals(sklearn_tf)
+
+
+def test_transformer_naming_override():
+
+    from ..transformers import StandardScaler
+    import pandas as pd
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    scaler = StandardScaler(name="test", keep_columns=False)
+    out = scaler.fit_transform(df[["crim"]])
+
+    assert out.iloc[:, 0].name == "crim_test_0"
+
+
+def test_transformer_naming_default():
+
+    from ..transformers import StandardScaler
+    import pandas as pd
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    scaler = StandardScaler(keep_columns=False)
+    out = scaler.fit_transform(df[["crim"]])
+
+    assert out.iloc[:, 0].name == "crim_StandardScaler_0"
+
+
+def test_transformer_arallel_invalid():
+
+    import pandas as pd
+
+    from ..transformers import ParallelProcessor
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    class InvalidTransformer:
+        pass
+
+    t = InvalidTransformer()
+
+    with pytest.raises(TypeError) as e:
+        proc = ParallelProcessor([("scaled", ["crim", "zn", "indus"], t)])
+
+    assert str(
+        e.value
+    ) == "All estimators should implement fit and " "transform. '%s' (type %s) doesn't" % (
+        t,
+        type(t),
+    )
+
+
+def test_transformer_parallel_empty():
+
+    import pandas as pd
+    import numpy as np
+
+    from ..transformers import ParallelProcessor
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    proc = ParallelProcessor([("scaled", ["crim", "zn", "indus"], None)])
+
+    proc.fit(df)
+    tf = proc.transform(df)
+
+    assert tf.equals(df[[]])
+
+    tf = proc.fit_transform(df)
+
+    assert tf.equals(df[[]])
+
+
+def test_transformer_parallel():
+
+    import pandas as pd
+
+    from ..transformers import ParallelProcessor
+    from ..transformers import StandardScaler
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    ss = StandardScaler(name="scaled")
+
+    proc = ParallelProcessor(
+        [("scaled", ["crim", "zn", "indus"], StandardScaler(keep_columns=False))]
+    )
+
+    ss.fit(df[["crim", "zn", "indus"]])
+    proc.fit(df)
+
+    tf = proc.transform(df)
+    tf_norm = ss.transform(df[["crim", "zn", "indus"]])
+
+    assert tf.equals(tf_norm)
+
+
+def test_transformer_pipeline():
+
+    import pandas as pd
+    import numpy as np
+
+    np.random.seed(1337)
+
+    from ..transformers import StandardScaler as CustomScaler
+    from ..transformers import ParallelProcessor
+
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.pipeline import FeatureUnion
+
+    from sklearn.pipeline import Pipeline
+    from sklearn.linear_model import LinearRegression
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    target = df["medv"]
+    df = df[["crim", "zn", "indus"]]
+    test = df.copy(deep=True)
+
+    custom = Pipeline(
+        [
+            (
+                "Step1",
+                ParallelProcessor(
+                    [
+                        (
+                            "scaled",
+                            ["crim", "zn", "indus"],
+                            CustomScaler(keep_columns=False),
+                        )
+                    ]
+                ),
+            ),
+            ("estimator", LinearRegression()),
+        ]
+    )
+
+    sklearn = Pipeline(
+        [
+            ("Step1", FeatureUnion([("scaled", StandardScaler())])),
+            ("estimator", LinearRegression()),
+        ]
+    )
+
+    sklearn.fit(df, target)
+    custom.fit(df, target)
+
+    print(custom.predict(test))
+    print(sklearn.predict(test))
+
+    assert np.array_equal(custom.predict(test), sklearn.predict(test))
+
+
+def test_smarttransformer_notimplemented():
+
+    import pandas as pd
+
+    from ..transformers import SmartTransformer
+    from ..transformers import StandardScaler
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    class TestSmartTransformer(SmartTransformer):
+        pass
+
+    transformer = TestSmartTransformer()
+
+    with pytest.raises(NotImplementedError) as e:
+        transformer.fit(df[["crim"]])
+
+    assert str(e.value) == "WrappedTransformer _get_transformer was not implimented."
+
+
+def test_smarttransformer_attributeerror():
+
+    import pandas as pd
+
+    from ..transformers import SmartTransformer
+    from ..transformers import StandardScaler
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    class TestSmartTransformer(SmartTransformer):
+        def _get_transformer(self, X, **fit_params):
+            return None
+
+    transformer = TestSmartTransformer()
+
+    with pytest.raises(AttributeError) as e:
+        transformer.fit(df[["crim"]])
+
+    assert (
+        str(e.value)
+        == "Invalid WrappedTransformer. Get transformer returns invalid object"
+    )
+
+
+def test_smarttransformer_invalidtransformer():
+
+    import pandas as pd
+
+    from ..transformers import SmartTransformer
+    from ..transformers import StandardScaler
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    class InvalidClass:
+        pass
+
+    class TestSmartTransformer(SmartTransformer):
+        def _get_transformer(self, X, **fit_params):
+            return InvalidClass()
+
+    transformer = TestSmartTransformer()
+
+    with pytest.raises(AttributeError) as e:
+        transformer.fit(df[["crim"]])
+
+    assert (
+        str(e.value)
+        == "Invalid WrappedTransformer. Get transformer returns invalid object"
+    )
+
+
+def test_smarttransformer_function():
+
+    import pandas as pd
+
+    from ..transformers import SmartTransformer
+    from ..transformers import StandardScaler
+
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
+    )
+
+    class TestSmartTransformer(SmartTransformer):
+        def _get_transformer(self, X, **fit_params):
+            return StandardScaler()
+
+    smart = TestSmartTransformer()
+    smart_data = smart.fit_transform(df[["crim"]])
+
+    std = StandardScaler()
+    std_data = std.fit_transform(df[["crim"]])
+
+    assert smart_data.equals(std_data)
+
+    smart.fit(df[["crim"]])
+    smart_data = smart.transform(df[["crim"]])
+
+    std.fit(df[["crim"]])
+    std_data = std.transform(df[["crim"]])
+
+    assert smart_data.equals(std_data)
--- foreshadow/transformers/__init__.py
+++ foreshadow/transformers/__init__.py
@@ -8,17 +8,28 @@ import pandas as pd
 from functools import partialmethod
 from scipy import sparse
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import FeatureUnion, _fit_one_transformer, _fit_transform_one, _transform_one
+from sklearn.pipeline import (
+    FeatureUnion,
+    _fit_one_transformer,
+    _fit_transform_one,
+    _transform_one,
+)
 from sklearn.externals.joblib import Parallel, delayed, Memory
 
 
 def get_classes():
     """Recursively searches transforms directory for classes"""
 
-    files = glob.glob(os.path.dirname(__file__)+"/*.py")
-    imports = [ os.path.basename(f)[:-3] for f in files if os.path.isfile(f) and not f.endswith('__init__.py')]
-    modules = [__import__(i, globals(), locals(), ['object'], 1) for i in imports]
-    classes = [ c[1] for m in modules for c in inspect.getmembers(m) if inspect.isclass(c[1]) ]
+    files = glob.glob(os.path.dirname(__file__) + "/*.py")
+    imports = [
+        os.path.basename(f)[:-3]
+        for f in files
+        if os.path.isfile(f) and not f.endswith("__init__.py")
+    ]
+    modules = [__import__(i, globals(), locals(), ["object"], 1) for i in imports]
+    classes = [
+        c[1] for m in modules for c in inspect.getmembers(m) if inspect.isclass(c[1])
+    ]
 
     return classes
 
@@ -28,8 +39,14 @@ def get_modules():
        extending sklearn.base.TransformerMixin
     """
 
-    transformers = [cls for cls in get_classes() if issubclass(cls, TransformerMixin) and issubclass(cls, BaseEstimator)]
-    for t in transformers: globals()[t.__name__] = wrap_transformer(t)
+    transformers = [
+        cls
+        for cls in get_classes()
+        if issubclass(cls, TransformerMixin) and issubclass(cls, BaseEstimator)
+    ]
+
+    for t in transformers:
+        globals()[t.__name__] = wrap_transformer(t)
 
     return len(transformers)
 
@@ -38,8 +55,14 @@ def wrap_transformer(transformer):
     """Wraps an sklearn transformer in functions allowing it to operate on a column basis
     """
 
-    members = [m[1] for m in inspect.getmembers(transformer, predicate=inspect.isfunction)]
-    wrap_candidates = [m for m in members if 'X' in inspect.getfullargspec(m).args and not m.__name__[0] == '_']
+    members = [
+        m[1] for m in inspect.getmembers(transformer, predicate=inspect.isfunction)
+    ]
+    wrap_candidates = [
+        m
+        for m in members
+        if "X" in inspect.getfullargspec(m).args and not m.__name__[0] == "_"
+    ]
 
     for w in wrap_candidates:
 
@@ -47,7 +70,11 @@ def wrap_transformer(transformer):
         setattr(transformer, w.__name__, partialmethod(pandas_wrapper, w))
 
     # Wrap constructor
-    setattr(transformer, '__init__', partialmethod(transform_constructor, transformer.__init__))
+    setattr(
+        transformer,
+        "__init__",
+        partialmethod(transform_constructor, transformer.__init__),
+    )
 
     return transformer
 
@@ -84,10 +111,10 @@ def pandas_wrapper(self, func, df, *args, **kwargs):
         pass
 
     init_cols = [col for col in df]
-    out = [func(self, df[[col]], *args, **kwargs) for col in df]
+    out = func(self, df, *args, **kwargs)
 
     # If output is numpy array (transform has occurred)
-    if isinstance(out[0], np.ndarray):
+    if isinstance(out, np.ndarray):
 
         # Remove old columns if necessary
         if not self.keep_columns:
@@ -100,9 +127,9 @@ def pandas_wrapper(self, func, df, *args, **kwargs):
             prefix = type(self).__name__
 
         # Append new columns to data frame
-        for k, o in enumerate(out):
-            for i, col in enumerate(o.transpose().tolist()):
-                df["{}_{}_{}".format(init_cols[k], prefix, i)] = pd.Series(col)
+        for i, col in enumerate(out.transpose().tolist()):
+            kw = {"{}_{}_{}".format("_".join(init_cols), prefix, i): pd.Series(col)}
+            df = df.assign(**kw)
 
         return df
 
@@ -110,13 +137,17 @@ def pandas_wrapper(self, func, df, *args, **kwargs):
 
 
 class ParallelProcessor(FeatureUnion):
-
     def __init__(self, transformer_list, n_jobs=1, transformer_weights=None):
 
         for i, item in enumerate(transformer_list):
-            transformer_list[i][2].name = transformer_list[i][0]
+            try:
+                transformer_list[i][2].name = transformer_list[i][0]
+            except AttributeError as e:
+                pass
 
-        super(ParallelProcessor, self).__init__(transformer_list, n_jobs, transformer_weights)
+        super(ParallelProcessor, self).__init__(
+            transformer_list, n_jobs, transformer_weights
+        )
 
     def _update_transformer_list(self, transformers):
         transformers = iter(transformers)
@@ -135,19 +166,24 @@ class ParallelProcessor(FeatureUnion):
         for t in transformers:
             if t is None:
                 continue
-            if (not (hasattr(t, "fit") or hasattr(t, "fit_transform")) or not
-                    hasattr(t, "transform")):
-                raise TypeError("All estimators should implement fit and "
-                                "transform. '%s' (type %s) doesn't" %
-                                (t, type(t)))
+            if not (hasattr(t, "fit") or hasattr(t, "fit_transform")) or not hasattr(
+                t, "transform"
+            ):
+                raise TypeError(
+                    "All estimators should implement fit and "
+                    "transform. '%s' (type %s) doesn't" % (t, type(t))
+                )
 
     def _iter(self):
         """Generate (name, est, weight) tuples excluding None transformers
         """
         get_weight = (self.transformer_weights or {}).get
-        return ((name, cols, trans, get_weight(name))
-                for name, cols, trans in self.transformer_list
-                if trans is not None)
+
+        return (
+            (name, cols, trans, get_weight(name))
+            for name, cols, trans in self.transformer_list
+            if trans is not None
+        )
 
     def fit(self, X, y=None):
         self.transformer_list = list(self.transformer_list)
@@ -155,45 +191,86 @@ class ParallelProcessor(FeatureUnion):
 
         transformers = Parallel(n_jobs=self.n_jobs)(
             delayed(_fit_one_transformer)(trans, X[cols], y)
-            for name, cols, trans, weight in self._iter())
+            for name, cols, trans, weight in self._iter()
+        )
 
         self._update_transformer_list(transformers)
+
         return self
 
     def transform(self, X):
 
         Xs = Parallel(n_jobs=self.n_jobs)(
             delayed(_transform_one)(trans, weight, X[cols])
-            for name, cols, trans, weight in self._iter())
+            for name, cols, trans, weight in self._iter()
+        )
 
         if not Xs:
             # All transformers are None
-            return np.zeros((X.shape[0], 0))
-        if any(sparse.issparse(f) for f in Xs):
-            Xs = sparse.hstack(Xs).tocsr()
+            return X[[]]
         else:
-            Xs = np.hstack(Xs)
+            Xs = pd.concat(Xs, axis=1)
         return Xs
 
     def fit_transform(self, X, y=None, **fit_params):
         self._validate_transformers()
 
         result = Parallel(n_jobs=self.n_jobs)(
-            delayed(_fit_transform_one)(trans, weight, X[cols], y,
-                                        **fit_params)
-            for name, cols, trans, weight in self._iter())
+            delayed(_fit_transform_one)(trans, weight, X[cols], y, **fit_params)
+            for name, cols, trans, weight in self._iter()
+        )
 
         if not result:
             # All transformers are None
-            return np.zeros((X.shape[0], 0))
+            return X[[]]
         Xs, transformers = zip(*result)
         self._update_transformer_list(transformers)
-        if any(sparse.issparse(f) for f in Xs):
-            Xs = sparse.hstack(Xs).tocsr()
-        else:
-            Xs = np.hstack(Xs)
+
+        Xs = pd.concat(Xs, axis=1)
         return Xs
 
+
+class SmartTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.transformer = None
+        super(SmartTransformer, self).__init__()
+
+    def _get_transformer(self, X, **fit_params):
+        raise NotImplementedError(
+            "WrappedTransformer _get_transformer was not implimented."
+        )
+
+    def _verify_transformer(self, X, **fit_params):
+
+        if self.transformer:
+            return
+
+        self.transformer = self._get_transformer(X, **fit_params)
+
+        if not self.transformer:
+            raise AttributeError(
+                "Invalid WrappedTransformer. Get transformer returns invalid object"
+            )
+
+        tf = getattr(self.transformer, "transform", None)
+        fittf = getattr(self.transformer, "fit_transform", None)
+        fit = getattr(self.transformer, "fit", None)
+
+        if not (callable(tf) and callable(fittf) and callable(fit)):
+            raise AttributeError(
+                "Invalid WrappedTransformer. Get transformer returns invalid object"
+            )
+
+    def fit_transform(self, X, y=None, **fit_params):
+        self._verify_transformer(X, **fit_params)
+        return self.transformer.fit_transform(X, y, **fit_params)
+
+    def transform(self, X, **kwargs):
+        return self.transformer.transform(X, **kwargs)
+
+    def fit(self, X, y=None, **kwargs):
+        self._verify_transformer(X)
+        return self.transformer.fit(X, y, **kwargs)
 
 
 # TODO: Determine verbosity of this process. Global?
