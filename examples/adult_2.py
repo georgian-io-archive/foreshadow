3eadee4742b0eb2f983a7aecba0da98f2e75257f
Adithya Balaji <adithyabsk@gmail.com>
2019-04-22 17:33:36 -0400

    Fix formatting issues

--- examples/adult_2.py
+++ examples/adult_2.py
@@ -7,12 +7,14 @@ import json
 
 RANDOM_SEED = 42
 
-adult = pd.read_csv('adult.csv').iloc[:1000]
+adult = pd.read_csv("adult.csv").iloc[:1000]
 
 print(adult.head())
-features = adult.drop(columns='class')
-target = adult[['class']]
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=RANDOM_SEED)
+features = adult.drop(columns="class")
+target = adult[["class"]]
+X_train, X_test, y_train, y_test = train_test_split(
+    features, target, test_size=0.2, random_state=RANDOM_SEED
+)
 
 # Load in the configs from file
 x_proc = json.load(open("adult_x_proc_search.json", "r"))
@@ -21,12 +23,16 @@ y_proc = json.load(open("adult_y_proc.json", "r"))
 # Create the preprocessors
 x_processor = fs.Preprocessor(from_json=x_proc)
 y_processor = fs.Preprocessor(from_json=y_proc)
-model = fs.Foreshadow(X_preprocessor=x_processor, y_preprocessor=y_processor,
-                      estimator=LogisticRegression(random_state=RANDOM_SEED), optimizer=GridSearchCV)
+model = fs.Foreshadow(
+    X_preprocessor=x_processor,
+    y_preprocessor=y_processor,
+    estimator=LogisticRegression(random_state=RANDOM_SEED),
+    optimizer=GridSearchCV,
+)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 print(classification_report(y_test, y_pred))
-print('Accuracy = %f' % accuracy_score(y_test, y_pred))
+print("Accuracy = %f" % accuracy_score(y_test, y_pred))
 
 # Serialize the pipeline
 x_proc = model.X_preprocessor.serialize()
@@ -35,4 +41,3 @@ y_proc = model.y_preprocessor.serialize()
 # Write the serialized pipelines to file
 json.dump(x_proc, open("adult_x_proc_searched.json", "w"), indent=4)
 json.dump(y_proc, open("adult_y_proc_searched.json", "w"), indent=4)
-
