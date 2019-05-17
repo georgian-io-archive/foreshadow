061c43ab369f3b395d8593f471cc2870ea55c582
Adithya Balaji <adithyabsk@gmail.com>
2019-05-17 13:49:03 -0400

    update readthedocs to v2 and fix macos script

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -37,13 +37,14 @@ if brew ls --versions pyenv-virtualenv > /dev/null; then
   echo pyenv-virutalenv is installed... skipping...
 else
   brew install pyenv-virtualenv
-  cat >> ~/.bash_profile <<EOF
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
+  cat >> ~/.bash_profile << EOF
+  # Setup pyenv
+  export PYENV_ROOT="\$HOME/.pyenv"
+  export PATH="\$PYENV_ROOT/bin:\$PATH"
   if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
+    eval "\$(pyenv init -)"
   fi
-  eval "$(pyenv virtualenv-init -)"
+  eval "\$(pyenv virtualenv-init -)"
 EOF
 fi
 
@@ -79,13 +80,11 @@ curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.p
 pyenv shell --unset
 
 # Add poetry to bash_profile
+cat >> ~/.bash_profile <<EOF
+
 # Setup poetry
-export PATH="$HOME/.poetry/bin:$PATH"
-source "$HOME/.poetry/env"
-  cat >> ~/.bash_profile <<EOF
-  # Setup poetry
-  export PATH="$HOME/.poetry/bin:$PATH"
-  source "$HOME/.poetry/env"
+export PATH="\$HOME/.poetry/bin:\$PATH"
+source "\$HOME/.poetry/env"
 EOF
 
 source ~/.bash_profile
