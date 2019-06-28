683c6ec53b8102ceba8cf83e6d996fb97944e78f
cchoquette <choquette.christopher@gmail.com>
2019-06-28 10:41:29 -0400

    Changes to pandas_wrapper of transformer to reduce code complexity.

--- foreshadow/transformers/transformers.py
+++ foreshadow/transformers/transformers.py
@@ -12,7 +12,16 @@ from sklearn.feature_extraction.text import VectorizerMixin
 from foreshadow.utils import check_df
 
 
-def _get_modules(classes, globals_, mname):
+def is_transformer(cls):
+    if issubclass(cls, BaseEstimator) and (
+            issubclass(cls, TransformerMixin) or
+            issubclass(cls, VectorizerMixin)
+    ):
+        return True
+    return False
+
+
+def _get_modules(classes, globals_, mname, wrap=True):
     """Import sklearn transformers from transformers directory.
 
     Searches transformers directory for classes implementing BaseEstimator and
@@ -24,25 +33,26 @@ def _get_modules(classes, globals_, mname):
         The list of wrapped transformers.
 
     """
-    transformers = [
-        cls
-        for cls in classes
-        if issubclass(cls, BaseEstimator)
-        and (
-            issubclass(cls, TransformerMixin)
-            or issubclass(cls, VectorizerMixin)
-        )
-    ]
+    transformers = []
+
+    for cls in classes:
+        if is_transformer(cls):
+            transformers.append(cls)
+
+    if wrap:  # wrap the transformer with the
+        transformer_wrapper = make_pandas_transformer
+    else:
+        transformer_wrapper = lambda x: x
 
     for t in transformers:
         copied_t = type(t.__name__, (t, *t.__bases__), dict(t.__dict__))
         copied_t.__module__ = mname
-        globals_[copied_t.__name__] = wrap_transformer(copied_t)
+        globals_[copied_t.__name__] = transformer_wrapper(copied_t)
 
     return [t.__name__ for t in transformers]
 
 
-def wrap_transformer(transformer):
+def make_pandas_transformer(transformer):
     """Wrap an sklearn transformer to support dataframes.
 
     Args:
@@ -53,23 +63,22 @@ def wrap_transformer(transformer):
         A wrapped form of transformer
 
     """
-    members = [
-        m[1]
-        for m in inspect.getmembers(transformer, predicate=inspect.isfunction)
-    ]
-    wrap_candidates = [
-        m
-        for m in members
-        if any(
-            name == m.__name__
-            for name in [
-                "fit",
-                "fit_transform",
-                "transform",
-                "inverse_transform",
-            ]
-        )
-    ]
+    declared_on = ['fit_transform']
+    exists_on = ['fit', 'transform', 'inverse_transform']
+    wrap_candidates = []
+    for m in transformer.__dict__.values():
+        if inspect.isfunction(m) and any(n == m.__name__ for n in declared_on):
+            wrap_candidates.append(m)
+
+    # normally, fit_transform will only be implemented in TransformerMixin
+    # and will not need to be wrapped. Only if it is on the transformer
+    # itself do we wrap it.
+    members = [m[1]
+               for m in inspect.getmembers(transformer,
+                                           predicate=inspect.isfunction)]
+    for m in members:
+        if any(name == m.__name__ for name in exists_on):
+            wrap_candidates.append(m)
 
     for w in wrap_candidates:
         # Wrap public function calls
@@ -180,10 +189,10 @@ def pandas_partial(func):  # noqa: D202
     """Partial function for the pandas transformer wrapper."""
 
     @wraps(func)
-    def wrapper(self, *args, **kwargs):
+    def pandas_func(self, *args, **kwargs):
         return pandas_wrapper(self, func, *args, **kwargs)
 
-    return wrapper
+    return pandas_func
 
 
 def init_replace(self, keep_columns=False, name=None):
@@ -217,7 +226,7 @@ class _Empty(BaseEstimator, TransformerMixin):
             :obj:`numpy.ndarray`: Empty numpy array
 
         """
-        return np.array([])
+        return pd.DataFrame([])
 
     def inverse_transform(self, X):
         """Pass through transform.
@@ -229,11 +238,70 @@ class _Empty(BaseEstimator, TransformerMixin):
             :obj:`numpy.ndarray`: Empty numpy array
 
         """
-        return np.array([])
+        return pd.DataFrame([])
 
 
-def _pandas_post_process(self):
-    pass
+def _keep_columns_process(out, dataframe, prefix):
+    """Keep original columns of input datafarme on output dataframe.
+
+    Args:
+        out: the output dataframe from the sklearn public function
+        dataframe: the input dataframe from the sklearn public function
+        prefix: the prefixes (name) to add
+
+    Returns:
+        [dataframe, out] concat along axis=1
+
+    """
+    dataframe.columns = [
+        "{}_{}_origin_{}".format(c, prefix, i)
+        for i, c in enumerate(dataframe.columns)
+    ]
+    return pd.concat([dataframe, out], axis=1)
+
+
+def _ndarray_post_process(ndarray, index, init_cols, prefix):
+    """Create dataframe from sklearn public function ndarray.
+
+    Args:
+        ndarray: the output ndarray from the sklearn public function
+        init_cols: the initial columns before public function call
+        prefix: prefix for each column (unique name)
+
+    Returns:
+        mimicked DataFrame for ndarray, with column names.
+
+    """
+    if ndarray.ndim == 1 and ndarray.size != 0:
+        ndarray = ndarray.reshape((-1, 1))
+
+    # Append new columns to data frame
+    kw = {}
+    for i, col in enumerate(ndarray.transpose().tolist()):
+        kw["{}_{}_{}".format("_".join(init_cols), prefix, i)] = pd.Series(
+                col, index=index
+            )
+
+    return pd.DataFrame(kw)
+
+
+def _df_post_process(dataframe, init_cols, prefix):
+    """Rename columns of output dataframe from sklearn public function.
+
+    Args:
+        dataframe: output DataFrame from sklearn public function
+        init_cols: the initial columns before public function call
+        prefix: prefix for each column (unique name)
+
+    Returns:
+        DataFrame with new column names
+
+    """
+    dataframe.columns = [
+        "{}_{}_{}".format("_".join(init_cols), prefix, c)
+        for c in dataframe.columns
+    ]
+    return dataframe
 
 
 def pandas_wrapper(self, func, df, *args, **kwargs):
@@ -265,83 +333,40 @@ def pandas_wrapper(self, func, df, *args, **kwargs):
         # transformer in a pipeline. Sklearn transformers will break on empty
         # input and so we reroute to _Empty.
         func = getattr(_Empty, func.__name__)
-
     try:
         out = func(self, df, *args, **kwargs)
-    except Exception:
+    except (TypeError, AttributeError):
         try:
             out = func(self, df, *args)
-        except Exception:
+        except (AttributeError, ValueError):
             from sklearn.utils import check_array
-
             dat = check_array(
                 df, accept_sparse=True, dtype=None, force_all_finite=False
-            ).tolist()
-            dat = [i for t in dat for i in t]
+            ).flatten()
             out = func(self, dat, *args)
 
-    # If output is DataFrame (custom transform has occured)
-    if isinstance(out, pd.DataFrame):
-        if hasattr(out, "from_transformer"):
-            return out
+    # determine name of new columns
+    name = self.name if self.name else type(self).__name__
+    out_is_transformer = hasattr(out, '__class__') and \
+                         is_transformer(out.__class__)  # check if the output
+    # returned by the sklearn public function is a transformer or not. It will
+    # be a transformer in fit calls.
 
-        if self.name:
-            prefix = self.name
+    if not (out_is_transformer):  #
+        # if the output is a transformer, we do nothing.
+        if isinstance(out, pd.DataFrame):  # custom handling based on the
+            # type returned by the sklearn transformer function call
+            out = _df_post_process(out, init_cols, name)
+        elif isinstance(out, np.ndarray):
+            out = _ndarray_post_process(out, df.index, init_cols, name)
+        elif scipy.sparse.issparse(out):
+            out = out.toarray()
+            out = _ndarray_post_process(out, df, init_cols, name)
+        elif isinstance(out, pd.Series):
+            pass  # just return the series
         else:
-            prefix = type(self).__name__
-
-        out.columns = [
-            "{}_{}_{}".format("_".join(init_cols), prefix, c)
-            for c in out.columns
-        ]
+            raise ValueError('undefined input {0}'.format(type(out)))
 
         if self.keep_columns:
-            df.columns = [
-                "{}_{}_origin_{}".format(c, prefix, i)
-                for i, c in enumerate(df.columns)
-            ]
-            return pd.concat([df, out], axis=1)
-
-        out.from_transformer = True
-
-        return out
-
-    if scipy.sparse.issparse(out):  # densify sparse matricies
-        out = out.toarray()
-
-    # If output is numpy array (transform has occurred)
-    if isinstance(out, np.ndarray):  # TODO can we abstract away this code,
-        # TODO combine as much as possible.
-
-        # Remove old columns if necessary
-        if not self.keep_columns:
-            df = df[[]]
-
-        # Determine name of new columns
-        if self.name:
-            prefix = self.name
-        else:
-            prefix = type(self).__name__
-
-        if out.ndim == 1 and out.size != 0:
-            out = out.reshape((-1, 1))
-
-        # Append new columns to data frame
-        for i, col in enumerate(out.transpose().tolist()):
-            kw = {
-                "{}_{}_{}".format("_".join(init_cols), prefix, i): pd.Series(
-                    col, index=df.index
-                )
-            }
-            df = df.assign(**kw)
-
-        df.from_transformer = True
-
-        return df
-
+            out = _keep_columns_process(out, df, name)
     return out
-
-
-# Wrap _Empty manually after function definition
-_Empty = wrap_transformer(_Empty)
-
