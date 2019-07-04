e96c90f73382f353c0faba9e4f4117ece4888cdd
cchoquette <choquette.christopher@gmail.com>
2019-07-04 14:59:59 -0400

    ColumnInfoSharer implementation: 1. interface and single-instance object 2. some tests

---
+++ foreshadow/column_sharer.py
@@ -0,0 +1,139 @@
+"""Cache utility for foreshadow pipeline workflow to share data"""
+from collections import defaultdict
+from collections import MutableMapping
+
+
+class ColumnSharer(MutableMapping):
+    """main cache-class to be used as single-instance to share data"""
+
+    def __init__(self, *args, **kwargs):
+        super(ColumnSharer, self).__init__(*args, **kwargs)
+        self.store = defaultdict(lambda: {})  # will have a nested dict for
+        # every key, which holds {column: key-column info}
+        acceptable_keys = {'intent': True,
+                           'domain': True,
+                           'metastat': True}
+        for key in acceptable_keys:
+            self.store[key] = {}
+        self.__acceptable_keys = defaultdict(lambda: False, acceptable_keys)
+        self.__registered_keys = defaultdict(lambda: False)
+
+    def __getitem__(self, key_list):
+        # first, get the item from the dict
+        key, column = self._convert_key(key_list)
+        self.check_key(key)
+        key_dict = self.store[key]
+        if column is not None:  # then get the column if requested
+            try:
+                return key_dict[column]
+            except Exception as e:
+                raise e
+        return key_dict  # otherwise return all the columns
+
+    def __setitem__(self, key_list, value):
+        key, column = self._convert_key(key_list)
+        self.check_key(key)
+        if column is not None:  # setting the value for the entire key
+            self.store[key] = value
+        else:  # setting a particular column's value for a given key.
+            self.store[key][column] = value
+
+    def check_key(self, key):
+        """Check they passed key to see if it is a valid key.
+
+        Args:
+            key: the key passed to this object.
+
+        Raises:
+            KeyError: if not a valid key (predefined or registered)
+
+        """
+        if not self.__acceptable_keys[key] and not self.__registered_keys[key]:
+            raise KeyError("key {} is not a valid key. Please register it "
+                           "first".format(key))
+        if self.__registered_keys[key]:
+            print("WARNING: the key {} is not an accepted key and relying on "
+                  "information here to exist at runtime could be "
+                  "dangerous".format(key))  # TODO replace with logging
+
+    @staticmethod
+    def _convert_key(key):
+        """Internal method to handle the different key input structures.
+
+        The supported key accessing methods are:
+            (str), where it is the type of data
+            (str, str), where it is (type of data, column)
+
+
+        Args:
+            key: the key passed to this object
+
+        Returns:
+            key, column based on the input format
+
+        Raises:
+            KeyError: if incorrect input format
+
+        """
+        if isinstance(key, str):  # technically doesn't need to be a string,
+            # just not an array type.
+            return key, None
+        elif isinstance(key, (list, tuple)):
+            if len(key) == 2:
+                return key[0], key[1]
+        raise KeyError("input format of: {} is not a supported "
+                       "format.".format(key))
+
+    def __delitem__(self, key_list):
+        key, column = self._convert_key(key_list)
+        self.check_key(key)
+        if column is None:
+            raise KeyError('You cannot delete an entire key of data. Please '
+                           'pass a key and a column.')
+        del self.store[key][column]
+
+    def __iter__(self):
+        """Will return list of (key, column) tuples ordered by key.
+
+        Column will be None if none exist, in line with the __getitem__
+        usage which will ignore it.
+
+        Returns:
+
+        """
+        # handle nested
+        iterator = []
+        for key in self.store:
+            key_iter = self.store[key].keys()
+            if len(key_iter) == 0:
+                iterator.extend([(key, None)])
+            else:
+                iterator.extend([(key, x) for x in key_iter])
+        return iterator
+
+    def __len__(self):
+        # handle nested
+        length = 0
+        for key in self.store:
+            length += len(self.store[key])
+        return length
+
+    def register_key(self, key):
+        """Register a non pre-defined key as acceptable.
+
+        Must be called before using a key unless it is in the list of
+        predefined acceptable keys (self.__acceptable_keys).
+
+        Args:
+            key: key to register
+
+        Raises:
+            KeyError: if key already set as predefined or registered
+
+        """
+        if self.__acceptable_keys[key]:
+            raise KeyError("key: '{}' is already a predefined key".format(key))
+        if self.__registered_keys[key]:
+            raise KeyError("key: '{}' is already a registered key".format(key))
+        self.store[key] = {}
+        self.__registered_keys[key] = True
---
+++ foreshadow/tests/test_column_sharer.py
@@ -0,0 +1,271 @@
+import pytest
+import re
+import pandas as pd
+from foreshadow.column_sharer import ColumnSharer
+
+
+simple_dataframe = pd.Series([i for i in range(10)])
+
+
+@pytest.mark.parametrize(
+    'args,kwargs',
+    [
+        ([], {}),
+    ]
+)
+def test_column_sharer_create(args, kwargs):
+    """Test arbitrary function can be converted to Metric using metric.
+
+    Args:
+        metric_fn: arbitrary metric function
+
+    """
+    from collections import MutableMapping
+    cs = ColumnSharer(*args, **kwargs)
+    assert isinstance(cs, MutableMapping)
+
+
+@pytest.mark.parametrize(
+    'key,expected_error,expected_str1,expected_str2',
+    [
+        ('testing', None, None, '.+already a registered key.+'),
+        ('domain',
+         KeyError,
+         '.+ already a predefined key.+',
+         '.+ already a predefined key.+'),
+    ]
+)
+def test_column_sharer_register(key, expected_error, expected_str1,
+                                expected_str2):
+    """Test registering key, ensuring key is on dict and error is raised.
+
+    Args:
+        metric_fn: arbitrary metric function
+        arg: arg to metric call
+        kwargs: any kwargs to metric call
+
+    """
+    cs = ColumnSharer()
+    if expected_error is not None:
+        with pytest.raises(expected_error) as e1:
+            cs.register_key(key)
+        with pytest.raises(expected_error) as e2:
+            cs.register_key(key)
+    else:
+        cs.register_key(key)
+    if expected_error is not None:
+        if expected_str1 is not None:
+            assert re.match(expected_str1, str(e1.value))
+        if expected_str2 is not None:
+            assert re.match(expected_str2, str(e2.value))
+    assert cs.store.get(key, None) is not None
+
+@pytest.mark.parametrize(
+    'key,expected_error1,expected_error2,expected_str1,expected_str2',
+    [
+        ('testing', None, KeyError,  None,'.+already a registered key.+'),
+        ('domain',
+         KeyError,
+         KeyError,
+         '.+ already a predefined key.+',
+         '.+ already a predefined key.+'),
+    ]
+)
+def test_column_sharer_register(key, expected_error1, expected_error2,
+                                expected_str1, expected_str2):
+    """Test registering key, ensuring key is on dict and error is raised.
+
+    Runs the register_key function twice as some errors only occur on the
+    second attempt.
+
+    Args:
+        key: key to register into ColumnSharer
+        expected_error1: None if no error on first try, otherwise the expected
+            error.
+        expected_error2: None if no error on the second try, otherwise the
+            expected error
+        expected_str1: expected string for the error on first attempt
+            expected
+        expected_str2: expected string for the error on second attempt
+            expected
+
+    """
+    cs = ColumnSharer()
+    if expected_error1 is not None:
+        with pytest.raises(expected_error1) as e1:
+            cs.register_key(key)
+    else:
+        cs.register_key(key)
+    if expected_error2 is not None:
+        with pytest.raises(expected_error2) as e2:
+            cs.register_key(key)
+    else:
+        cs.register_key(key)
+    if expected_error1 is not None:
+        if expected_str1 is not None:
+            assert re.match(expected_str1, str(e1.value))
+    if expected_error2 is not None:
+        if expected_str2 is not None:
+            assert re.match(expected_str2, str(e2.value))
+    assert cs.store.get(key, None) is not None
+
+
+@pytest.mark.parametrize(
+    'key,expected',
+    [
+        ('test_key', ('test_key', None)),
+        (['test_key', 'test_column'], ('test_key', 'test_column')),
+        (['1', '2', '3'], KeyError)
+    ]
+)
+def test_column_sharer_convert_key(key, expected):
+    """Test that key conversion and error raising works as expected.
+
+    Args:
+        key: key to register into ColumnSharer
+        expected: error if error, result if result.
+
+    """
+    cs = ColumnSharer()
+    try:  # assume expected is an error
+        if issubclass(expected, BaseException):  # this will fail if it isn't
+            with pytest.raises(expected) as e:
+                cs._convert_key(key)
+            assert issubclass(e.type, expected)
+    except TypeError:  # then expected will be the true result returned
+        assert cs._convert_key(key) == expected
+
+
+@pytest.mark.parametrize(
+    'key,item_to_set,expected',
+    [
+        (['domain'], {}, {}),
+        (['domain', 'column'], {'column': 1}, 1),
+        (['domain', 'column'], {}, KeyError),  # can't get column info if
+        # there is no column info
+        (['domain', None], {}, {})
+    ]
+)
+def test_column_sharer_getitem(key, item_to_set, expected):
+    """Test that getitem works for all valid key combinations or error raised.
+
+    Args:
+        key (list): key to access on ColumnSharer
+        item_to_set: the item to set on the key as starting data. Dependent
+            on the length of the key.
+        expected: the expected result or error
+
+    """
+    cs = ColumnSharer()
+    if len(key) == 1:
+        cs.store[key[0]] = item_to_set
+        try:  # assume expected is an error
+            if issubclass(expected,
+                          BaseException):  # this will fail if it isn't
+                with pytest.raises(expected) as e:
+                    cs[key[0]]
+                assert issubclass(e.type, expected)
+        except TypeError:  # then expected will be the true result returned
+            assert cs[key[0]] == expected
+
+    elif len(key) == 2:
+        cs.store[key[0]] = item_to_set
+        try:  # assume expected is an error
+            if issubclass(expected,
+                          BaseException):  # this will fail if it isn't
+                with pytest.raises(expected) as e:
+                    cs[key[0], key[1]]
+                assert issubclass(e.type, expected)
+        except TypeError:  # then expected will be the true result returned
+            assert cs[key[0], key[1]] == expected
+
+    else:
+        raise NotImplementedError('test case not implemented')
+
+
+@pytest.mark.parametrize(
+    'key,key_to_register,expected',
+    [
+        ('domain', None, None),
+        ('test', 'test', None),
+        ('test', None, KeyError)  # can't get column info if
+        # there is no column info
+    ]
+)
+def test_column_sharer_checkkey(key, key_to_register, expected):
+    """Test that getitem works for all valid key combinations or error raised.
+
+    Args:
+        key (list): key to access on ColumnSharer
+        key_to_register: key to register as starting info
+        expected: the expected result or error
+
+    """
+    cs = ColumnSharer()
+    if key_to_register is not None:
+        cs.register_key(key_to_register)
+    try:
+        expected_is_exception = issubclass(expected, BaseException)  # this
+        # will fail if expected is not an Exception
+    except TypeError:
+        expected_is_exception = False
+    if expected_is_exception:
+        with pytest.raises(expected) as e:
+            cs.check_key(key)
+        assert issubclass(e.type, expected)
+    else:
+        cs.check_key(key)
+
+
+@pytest.mark.parametrize(
+    'key,expected',
+    [
+        ('domain', KeyError),
+        (('domain', 'haha'), KeyError),
+        (('domain', 'test'), None)
+    ]
+)
+def test_column_sharer_delitem(key, expected):
+    """Test that delitem works for all valid key combinations or error raised.
+
+    Args:
+        key (list): key to delete on ColumnSharer
+        expected: the expected result or error
+
+    """
+    cs = ColumnSharer()
+    cs.store['domain'] = {'test': True}
+    if len(key) == 1:
+        with pytest.raises(expected) as e:
+            del cs[key]
+        assert isinstance(e.type, expected)
+
+    if len(key) == 2:
+        if expected is not None:
+            with pytest.raises(expected) as e:
+                del cs[key[0], key[1]]
+        else:
+            del cs[key[0], key[1]]
+
+
+@pytest.mark.parametrize(
+    'store',
+    [
+        {'domain': {}, 'intent': {}, 'metastat': {}}
+    ]
+)
+def test_column_sharer_iter(store):
+    """Test that iter iterates over entire internal dict properly.
+
+    Args:
+        store: the internal dictionary to use.
+
+    """
+    cs = ColumnSharer()
+    cs.store = store
+
+    expected = {}  # we try to recreate the internal dict using the keys
+
+    for key in iter(cs.store):
+        expected[key] = cs.store[key]
+    assert expected == cs
