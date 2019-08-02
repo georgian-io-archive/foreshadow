a96f841f21ac2c89baa5e0bdb6d3ecf53658af1e
Christopher Choquette Choo <choquette.christopher@gmail.com>
2019-08-02 16:20:12 -0400

    More flake.

--- foreshadow/concrete/internals/__init__.py
+++ foreshadow/concrete/internals/__init__.py
@@ -9,7 +9,7 @@ from foreshadow.concrete.internals.dummyencoder import (  # noqa: F403, F401
 )
 from foreshadow.concrete.internals.fancyimpute import (  # noqa: F403, F401
     FancyImputer,
-)  # noqa: F401
+)
 from foreshadow.concrete.internals.financial import (  # noqa: F401
     ConvertFinancial,
     PrepareFinancial,
@@ -17,15 +17,15 @@ from foreshadow.concrete.internals.financial import (  # noqa: F401
 from foreshadow.concrete.internals.htmlremover import HTMLRemover  # noqa: F401
 from foreshadow.concrete.internals.labelencoder import (  # noqa: F403, F401
     FixedLabelEncoder,
-)  # noqa: F401
+)
 from foreshadow.concrete.internals.notransform import NoTransform  # noqa: F401
 from foreshadow.concrete.internals.tfidf import (  # noqa: F403, F401
     FixedTfidfVectorizer,
-)  # noqa: F401
+)
 from foreshadow.concrete.internals.tostring import ToString  # noqa: F401
 from foreshadow.concrete.internals.uncommonremover import (  # noqa: F403, F401
     UncommonRemover,
-)  # noqa: F401
+)
 
 
 __all__ = [
