12422f8235c193d8b79e48728a7beed9d587704b
Adithya Balaji <adithyabsk@gmail.com>
2019-08-06 10:15:41 -0400

    Add docstrings

--- foreshadow/config.py
+++ foreshadow/config.py
@@ -1,29 +1,31 @@
 """Foreshadow system config resolver."""
 
+import json
 import os
+from collections import MutableMapping
 
 import yaml
-import json
 
 from foreshadow.utils import get_config_path, get_transformer
-
-from collections import MutableMapping
 
 
 CONFIG_FILE_NAME = "config.yml"
 
-# TODO write a singleton object for this dictionary
-
-DEFAULT_CONFIG = {
+_DEFAULT_CONFIG = {
     "Cleaner": {
-        'Flatteners': ['StandardJsonFlattener'],
-        'Cleaners': ['YYYYMMDDDateCleaner', 'DropCleaner', 'DollarFinancialCleaner']
+        "Flatteners": ["StandardJsonFlattener"],
+        "Cleaners": [
+            "YYYYMMDDDateCleaner",
+            "DropCleaner",
+            "DollarFinancialCleaner",
+        ],
     },
     "Tiebreak": ["Numeric", "Categoric", "Text"],
     "Numeric": {"Preprocessor": ["Imputer", "Scaler"]},
     "Categoric": {"Preprocessor": ["CategoricalEncoder"]},
     "Text": {"Preprocessor": ["TextEncoder"]},
 }
+
 
 def load_config(base):
     """Try to load configuration data from specific folder path.
@@ -50,30 +52,37 @@ def load_config(base):
             else:
                 return data
 
+
 class ConfigStore(MutableMapping):
-    """Defines a single-instance config store with convenience methods."""
+    """Define a single-instance config store with convenience methods.
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    Attributues:
+        system_config: The default system configuration dictionary
+        user_config: The specific user configuration dictionary
 
-        self.system_config = DEFAULT_CONFIG
+    """
+
+    def __init__(self, *args, **kwarg):
+        self.system_config = _DEFAULT_CONFIG
         self.user_config = load_config(get_config_path())
-        self._cfg_list = {} # key is path
+        self._cfg_list = {}  # key is path
 
     def get_config(self):
-        """Resolves config at init time."""
+        """Resolve a config instance.
+
+        Returns:
+            dict: A resolved version of the system configuration that merges \
+                system, user, and local configuration setups.
+
+        """
         local_path = os.path.abspath("")
         local_config = load_config(local_path)
-
-        # global _cfg
-        # if local_path in _cfg:
-        #     return _cfg.get(local_path)
 
         # Expand the dictionaries in order of precedence
         resolved_strs = {
             **self.system_config,
             **self.user_config,
-            **local_config
+            **local_config,
         }
 
         resolved_hash = hash(json.dumps(resolved_strs, sort_keys=True))
@@ -105,31 +114,90 @@ class ConfigStore(MutableMapping):
         return resolved
 
     def get_intents(self):
-        return self.get_config()['Tiebreak']
+        """Get the intent resolution order.
+
+        Returns:
+            list: A list of intent objects in order
+
+        """
+        return self.get_config()["Tiebreak"]
 
     def get_preprocessor_steps(self, intent):
-        return self.get_config()[intent]['Preprocessor']
+        """Get the preprocessor list for a given intent.
+
+        Args:
+            intent: A string of the intent to select upon.
+
+        Returns:
+            list: A list of transformation classes for an intent
+
+        """
+        return self.get_config()[intent]["Preprocessor"]
 
     def clear(self):
+        """Clear all cached configuration stores."""
         self._cfg_list = {}
 
     def __delitem__(self, key):
+        """Delete an item from the config cache for a given hash value.
+
+        Args:
+            key: A hash value for the item to delete
+
+        """
         del self._cfg_list[key]
 
     def __getitem__(self, key):
+        """Get an item from the config cache for a given hash value.
+
+        Args:
+            key: A hash value for the item to get
+
+        Returns:
+            dict: The configuration for a particular hash value.
+
+        """
         return self._cfg_list[key]
 
     def __iter__(self):
+        """Get the iterable the config cache.
+
+        Yields:
+            The key, value pairs of hash and its associated configuration.
+
+        """
         for data in self._cfg_list:
             yield data
 
     def __len__(self):
+        """Get the number of hashes saved in the cache.
+
+        Returns:
+            The number of hashes saved in the internal cache.
+
+        """
         return len(self._cfg_list)
 
     def __setitem__(self):
+        """Values cannot be set in the cache.
+
+        Raises:
+            NotImplementedError: The config cannot be manually set.
+
+        """
         raise NotImplementedError("The config cannot be manually set.")
 
     def __eq__(self, other):
+        """Check the equality of the cache with another cache instance.
+
+        Args:
+            other: Another cache instance or dictionary.
+
+        Returns:
+            bool: True if the caches are equal, False otherwise
+
+        """
         return self._cfg_list == other
+
 
 config = ConfigStore()
