0d740dcd9e4748c44b68fc3b8b98626a568d8cb0
Jing Zhang <jzhang@georgianpartners.com>
2019-11-21 14:02:16 -0500

    Clean up, fix and adjust a few skipped unit tests and remove outdated features in the console script.

--- foreshadow/console.py
+++ foreshadow/console.py
@@ -88,18 +88,6 @@ def process_argument(args):  # noqa: C901
         help="Time limit in minutes to apply to model"
         "parameter search. (Default 10)",
     )
-    parser.add_argument(
-        "--x_config",
-        default=None,
-        type=str,
-        help="Path to JSON configuration file for X Preprocessor",
-    )
-    parser.add_argument(
-        "--y_config",
-        default=None,
-        type=str,
-        help="Path to JSON configuration file for y Preprocessor",
-    )
     cargs = parser.parse_args(args)
 
     return cargs
@@ -264,15 +252,6 @@ def execute_model(fs, X_train, y_train, X_test, y_test):
         "Serialized foreshadow pipeline has been saved to foreshadow.json. "
         "Refer to docs to read and process."
     )
-    # TODO serialize the foreshadow object and summarize the X and y stats.
-    # Store final results
-    # all_results = {
-    #     "X_Model": fs.X_preparer.serialize(),
-    #     # "X_Summary": fs.X_preparer.summarize(X_train),
-    #     "y_Model": fs.y_preparer.serialize(),
-    #     # "y_summary": fs.y_preparer.summarize(y_train),
-    # }
-    # return all_results
 
 
 def cmd():  # pragma: no cover
