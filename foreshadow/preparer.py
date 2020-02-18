cca73439e5143a9ddf5c03e684929fae2f95ed70
Jing Zhang <jzhang@georgianpartners.com>
2020-02-18 14:00:45 -0500

    Update the data exporter

--- foreshadow/preparer.py
+++ foreshadow/preparer.py
@@ -10,6 +10,7 @@ from foreshadow.serializers import (
 from foreshadow.smart import CategoricalEncoder
 from foreshadow.steps import (
     CleanerMapper,
+    DataExporterMapper,
     FeatureSummarizerMapper,
     IntentMapper,
     Preprocessor,
@@ -102,9 +103,9 @@ class DataPreparer(
         # # reducer_kwargs_ = _none_to_dict(
         # #     "reducer_kwargs", reducer_kwargs, cache_manager
         # # )
-        # exporter_kwargs_ = _none_to_dict(
-        #     "exporter_kwargs", exporter_kwargs, cache_manager
-        # )
+        exporter_kwargs_ = _none_to_dict(
+            "exporter_kwargs", exporter_kwargs, cache_manager
+        )
         if not y_var:
             steps = [
                 ("data_cleaner", CleanerMapper(**cleaner_kwargs_)),
@@ -119,7 +120,7 @@ class DataPreparer(
                 # ),
                 ("feature_preprocessor", Preprocessor(**preprocessor_kwargs_)),
                 # ("feature_reducer", FeatureReducerMapper(**reducer_kwargs_)),
-                # ("feature_exporter", DataExporterMapper(**exporter_kwargs_)),
+                ("feature_exporter", DataExporterMapper(**exporter_kwargs_)),
             ]
         else:
             if problem_type == ProblemType.REGRESSION:
--- foreshadow/steps/data_exporter.py
+++ foreshadow/steps/data_exporter.py
@@ -1,7 +1,7 @@
 """PrepareStep that exports the processed data before sending to Estimator."""
 from foreshadow.logging import logging
 from foreshadow.smart import DataExporter
-from foreshadow.utils import ConfigKey, DefaultConfig
+from foreshadow.utils import AcceptedKey, ConfigKey, DefaultConfig
 
 from .autointentmap import AutoIntentMixin
 from .preparerstep import PreparerStep
@@ -32,25 +32,39 @@ class DataExporterMapper(PreparerStep, AutoIntentMixin):
             cols=X.columns,
         )
 
-    def fit_transform(self, X, y=None, **fit_params):
+    def fit(self, X, *args, **kwargs):
+        """Fit this step.
+
+        calls underlying parallel process.
+
+        Args:
+            X: input DataFrame
+            *args: args to _fit
+            **kwargs: kwargs to _fit
+
+        Returns:
+            transformed data handled by Pipeline._fit
+
+        """
+        return self
+
+    def fit_transform(self, X, *args, **kwargs):
         """Fit then transform a dataframe.
 
         Side-affect: export the dataframe to disk as a csv file.
 
         Args:
             X: input DataFrame
-            y: input labels
-            **fit_params: kwarg params to fit
+            *args: args to _fit
+            **kwargs: kwargs to _fit
 
         Returns:
             Result from .transform(), pass through.
 
         """
-        Xt = super().fit_transform(X, y, **fit_params)
-        self._export_data(Xt, is_train=True)
-        return Xt
+        return self.fit(X, *args, **kwargs).transform(X, is_train=True)
 
-    def transform(self, X, *args, **kwargs):
+    def transform(self, X, *args, is_train=False, **kwargs):
         """Transform a dataframe.
 
         Side-affect: export the dataframe to disk as a csv file.
@@ -58,15 +72,16 @@ class DataExporterMapper(PreparerStep, AutoIntentMixin):
         Args:
             X: input DataFrame
             *args: args to .transform()
+            is_train: whether this is training or testing process
             **kwargs: kwargs to .transform()
 
         Returns:
             Result from .transform(), pass through.
 
         """
-        Xt = super().transform(X, *args, **kwargs)
-        self._export_data(Xt, is_train=False)
-        return Xt
+        # Xt = super().transform(X, *args, **kwargs)
+        self._export_data(X, is_train=is_train)
+        return X
 
     def _handle_intent_override(self, default_parallel_process):
         """Handle intent override and see override in the child classes.
@@ -85,19 +100,19 @@ class DataExporterMapper(PreparerStep, AutoIntentMixin):
         X.to_csv(data_path, index=False)
         logging.info("Exported processed data to {}".format(data_path))
 
-    def _determine_export_path(self, is_train=True):
+    def _determine_export_path(self, is_train):
         key_to_check = (
             ConfigKey.PROCESSED_TRAINING_DATA_EXPORT_PATH
             if is_train
             else ConfigKey.PROCESSED_TEST_DATA_EXPORT_PATH
         )
 
-        if key_to_check not in self.cache_manager["config"]:
+        if key_to_check not in self.cache_manager[AcceptedKey.CONFIG]:
             data_path = (
                 DefaultConfig.PROCESSED_TRAINING_DATA_EXPORT_PATH
                 if is_train
                 else DefaultConfig.PROCESSED_TEST_DATA_EXPORT_PATH
             )
         else:
-            data_path = self.cache_manager["config"][key_to_check]
+            data_path = self.cache_manager[AcceptedKey.CONFIG][key_to_check]
         return data_path
