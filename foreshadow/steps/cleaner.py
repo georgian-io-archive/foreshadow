68675143497854b6e15f8aa4bf4fc8cca78cf7d4
Jing Zhang <jzhang@georgianpartners.com>
2020-03-05 14:00:07 -0500

    Cleanup the cleaner step in foreshadow

--- foreshadow/steps/cleaner.py
+++ foreshadow/steps/cleaner.py
@@ -10,38 +10,6 @@ from foreshadow.utils import AcceptedKey, ConfigKey
 from .preparerstep import PreparerStep
 
 
-# def _check_empty_columns(X) -> NoReturn:
-#     """Check if all columns are empty in the dataframe.
-#
-#     Args:
-#         X: the dataframe
-#
-#     Returns:
-#         the empty columns.
-#
-#     Raises:
-#         ValueError: all columns are dropped.
-#
-#     """
-#     columns = pd.Series(X.columns)
-#     empty_columns = columns[X.isnull().all(axis=0).values]
-#
-#     if len(empty_columns) == len(columns):
-#         error_message = (
-#             "All columns are dropped since they all have "
-#             "over 90% of missing values. Aborting foreshadow."
-#         )
-#         logging.error(error_message)
-#         raise ValueError(error_message)
-#     elif len(empty_columns) > 0:
-#         logging.info(
-#             "Identified columns with over 90% missing values: {}"
-#             "".format(",".join(empty_columns.tolist()))
-#         )
-#
-#     return empty_columns.tolist()
-
-
 class CleanerMapper(PreparerStep):
     """Determine and perform best data cleaning step."""
 
@@ -69,14 +37,39 @@ class CleanerMapper(PreparerStep):
             transformed data handled by Pipeline._fit
 
         """
+        self._prepare_feature_processor(X=X)
+        self.feature_processor.fit(X=X)
+        self._empty_columns = self._check_empty_columns(
+            original_columns=X.columns
+        )
+        return self
+
+    def transform(self, X, *args, **kwargs):
+        """Clean the dataframe.
+
+        Args:
+            X: the data frame.
+            *args: positional args.
+            **kwargs: key word args.
+
+        Returns:
+            A transformed dataframe.
+
+        Raises:
+            ValueError: Cleaner has not been fitted.
+
+        """
+        if self._empty_columns is None:
+            raise ValueError("Cleaner has not been fitted yet.")
+
+        Xt = self.feature_processor.transform(X=X)
+        return Xt.drop(columns=self._empty_columns)
+
+    def _prepare_feature_processor(self, X):
         columns = X.columns
         list_of_tuples = [
             (
-                column,
-                # make_pipeline(
-                #     Flatten(cache_manager=self.cache_manager),
-                #     Cleaner(cache_manager=self.cache_manager),
-                # ),
+                column + "_" + Cleaner.__class__.__name__,
                 Cleaner(cache_manager=self.cache_manager),
                 column,
             )
@@ -86,11 +79,6 @@ class CleanerMapper(PreparerStep):
             list_of_tuples,
             n_jobs=self.cache_manager[AcceptedKey.CONFIG][ConfigKey.N_JOBS],
         )
-        self.feature_processor.fit(X=X)
-        self._empty_columns = self._check_empty_columns(
-            original_columns=columns
-        )
-        return self
 
     def _check_empty_columns(self, original_columns: List) -> List:
         empty_columns = []
@@ -114,44 +102,3 @@ class CleanerMapper(PreparerStep):
             )
 
         return empty_columns
-
-    def fit_transform(self, X, *args, **kwargs):
-        """Fit then transform the cleaner step.
-
-        Args:
-            X: the data frame.
-            *args: positional args.
-            **kwargs: key word args.
-
-        Returns:
-            A transformed dataframe.
-
-        """
-        return self.fit(X, *args, **kwargs).transform(X)
-        # Xt = super().fit_transform(X, *args, **kwargs)
-        # self._empty_columns = _check_empty_columns(Xt)
-        # return Xt.drop(columns=self._empty_columns)
-
-    def transform(self, X, *args, **kwargs):
-        """Clean the dataframe.
-
-        Args:
-            X: the data frame.
-            *args: positional args.
-            **kwargs: key word args.
-
-        Returns:
-            A transformed dataframe.
-
-        Raises:
-            ValueError: Cleaner has not been fitted.
-
-        """
-        if self._empty_columns is None:
-            raise ValueError("Cleaner has not been fitted yet.")
-
-        # Xt = super().transform(X, *args, **kwargs)
-
-        Xt = self.feature_processor.transform(X=X)
-        # Xt = pd.DataFrame(data=Xt, columns=X.columns)
-        return Xt.drop(columns=self._empty_columns)
